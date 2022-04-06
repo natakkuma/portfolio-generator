bb9cddefa2445d4f5e72ae6bb211ffcf65318ef4
natakkuma <95733427+natakkuma@users.noreply.github.com>
2022-04-06 14:32:05 -0700

    page template and app js to generate HTML file

--- app.js
+++ app.js
@@ -1,16 +1,35 @@
-const profileDataArgs = process.argv.slice(2);
+//Lesson 9.1
+
+//const profileDataArgs = process.argv.slice(2);
 
 //Using New arrow function syntax
-const printProfileData = profileDataArr => {
+//const printProfileData = profileDataArr => {
     // This...
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-      console.log(profileDataArr[i]);
-    }
+    //for (let i = 0; i < profileDataArr.length; i += 1) {
+      //console.log(profileDataArr[i]);
+    //}
   
-    console.log('================');
+    //console.log('================');
   
     // Is the same as this...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-  };
+    //profileDataArr.forEach(profileItem => console.log(profileItem));
+  //};
   
-printProfileData(profileDataArgs);
+//printProfileData(profileDataArgs);
+
+//Lesson 9.2
+
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+
+const profileDataArgs = process.argv.slice(2);
+//const name = profileDataArgs[0];
+//const github = profileDataArgs[1];
+//replace above code with shorter version below
+const [name, github] = profileDataArgs;
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+  if (err) throw err;
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
